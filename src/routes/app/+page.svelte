d63b6715fe192ac10f2d3666ef2460adac41d4ba
smoyamedina <smoyamedina@outlook.com>
2023-07-21 07:03:04 -0400

    Use proper components after importing new files

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -2,7 +2,10 @@
 	import TunerSettings from '$lib/components/TunerSettings.svelte';
 	import Dropdown from '$lib/components/micro/Dropdown.svelte';
 	import Text from '$lib/components/micro/Text.svelte';
+	import TextArea from '$lib/components/micro/TextArea.svelte';
+	import InputWrapper from '$lib/components/nano/InputWrapper.svelte';
 	import Option from '$lib/components/nano/Option.svelte';
+	import Radio from '$lib/components/micro/Radio.svelte';
 
 	let clientName = 'Friends of the Earth';
 </script>
@@ -20,30 +23,34 @@
 			<h2 class="tuner__heading typelvl-b">Settings</h2>
 			<div class="tuner__inputs">
 				<TunerSettings groupname="Base Information">
-					<Text id="slug" label="This is the input label..." />
+					<InputWrapper type="isw--radio-group">
+						<Radio radioGroup="emailType" id="fr" label="Fundraising" />
+						<Radio radioGroup="emailType" id="bu" label="Bundling" />
+						<Radio radioGroup="emailType" id="su" label="Survey or Petition" />
+						<Radio radioGroup="emailType" id="ot" label="Other" />
+					</InputWrapper>
+					<Text id="slug" label="Donation Page Slug" />
 					<Text id="previewtext" label="What is your preview text?" />
 				</TunerSettings>
 
 				<TunerSettings groupname="Ask Customization">
 					<Dropdown id="hpc" label="What HPC level is needed?">
-						<Option options={['two', 'three', 'four']} />
+						<Option options={[15, 27, 50, 100]} />
 					</Dropdown>
 					<Text id="TESTGO44OD" label="TTESTY" />
 				</TunerSettings>
 			</div>
 		</div>
-		<div class="t__codeareas">
+		<div class="t__codewrapper">
 			<fieldset class="fieldset ca_fieldset">
-				<h2 class="codearea__heading typelvl-b">Replace</h2>
+				<h2 class="codearea__heading typelvl-b">Replacement</h2>
 				<div class="code-wrapper">
 					<div class="input-card code-area">
-						<label class="typelvl-c" for="inputBtn">Input</label>
-						<textarea name="inputBtn" id="inputText" class="inputText" />
+						<TextArea id="input" label="Input" />>
 						<button id="inputBtn" class="inputBtn">Update my HTML</button>
 					</div>
 					<div class="output-card code-area">
-						<label class="typelvl-c" for="outputBtn">Output</label>
-						<textarea name="output" id="outputText" class="outputText" />
+						<TextArea id="output" label="Output" />->
 						<button id="outputBtn" class="outputBtn">Copy new HTML</button>
 					</div>
 				</div>
