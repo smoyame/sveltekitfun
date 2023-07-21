66768ff843fe113d45828d141ab903a484010d03
smoyamedina <smoyamedina@outlook.com>
2023-07-21 00:52:46 -0400

    Micro components

---
+++ src/lib/components/nano/InputWrapper.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let type: string;
+</script>
+
+<div class="input-wrapper input__{type}">
+	<slot />
+</div>
---
+++ src/lib/components/nano/Option.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let options: Array<any> = ['First', 'Second', 'Third'];
+</script>
+
+{#each options as option}
+	<option value={option.toLowerCase()}>{option}</option>
+{/each}
