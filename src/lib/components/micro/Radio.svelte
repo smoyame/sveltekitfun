d2746d0c945e2e442fd3d249c0b4f7bac069893a
smoyamedina <smoyamedina@outlook.com>
2023-07-21 07:01:23 -0400

    fix name and grouping

--- src/lib/components/micro/Radio.svelte
+++ src/lib/components/micro/Radio.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import InputWrapper from '$lib/components/nano/InputWrapper.svelte';
 	export let id: string;
+	export let radioGroup: string;
 	export let label: string;
 </script>
 
 <InputWrapper type="radio">
 	<label for={id} class={'label--settings ls__' + id}>{label}</label>
-	<input type="radio" name={id} {id} class={id + ' radio'} value={label} />
+	<input type="radio" name={radioGroup} {id} class={id + ' radio'} value={label.toLowerCase()} />
 </InputWrapper>
