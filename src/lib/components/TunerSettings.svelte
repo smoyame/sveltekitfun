ad730c8ce434ea3bb5c1c25165892d4e6996188b
smoyamedina <smoyamedina@outlook.com>
2023-07-21 00:54:00 -0400

    Components

---
+++ src/lib/components/TunerSettings.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let groupname: string;
+</script>
+
+<fieldset class="fieldset t__fieldset">
+	<h3 class="fset__heading typelvl-c">{groupname}</h3>
+	<div class="inputs-wrapper">
+		<slot />
+	</div>
+</fieldset>
